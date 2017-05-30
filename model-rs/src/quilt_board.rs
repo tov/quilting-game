1f3fac6aedee722f0dc5503d3120da74e140cef8
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-30 10:08:36 -0500

    QuiltBoard methods.

--- model-rs/src/quilt_board.rs
+++ model-rs/src/quilt_board.rs
@@ -1,32 +1,80 @@
 use piece::Piece;
+use position::{Position, Dimension, Transformation};
 
+/// The board on which the quilt is constructed.
+///
+/// Invariant:
+///
+///  - rows.len() == height
+///
+///  - for row in rows { row.len() == width }
 pub struct QuiltBoard {
-    width:  usize,
-    height: usize,
-    rows:   Box<[Box<[bool]>]>,
+    dimension: Dimension,
+    rows:      Box<[Box<[bool]>]>,
 }
 
 impl QuiltBoard {
-    pub fn invariant(&self) -> bool {
+    /// Creates a new, empty board of the given dimensions.
+    pub fn new(width: usize, height: usize) -> Self {
+        let mut rows = Vec::new();
+
+        for _ in 0 .. height {
+            rows.push(vec![false; width].into_boxed_slice());
+        }
+
+        QuiltBoard {
+            dimension: Dimension::new(width, height),
+            rows:      rows.into_boxed_slice(),
+        }
+    }
+
+    /// Returns the number of squares covered by pieces.
+    pub fn squares_covered(&self) -> usize {
+        let mut result = 0;
+
         for row in &*self.rows {
-            if row.len() != self.width {
-                return false;
+            for &b in &**row {
+                if b { result += 1; }
             }
         }
 
-        self.rows.len() == self.height
+        result
     }
 
-    pub fn can_add_piece(&self, x: usize, y: usize, piece: Piece) -> bool {
-//        if piece.width() + x > self.width {
-//            false
-//        } else {
-//            false
-//        }
-        false
+    /// Can the given piece be added at the given position under the given transformation?
+    ///
+    /// Returns `Err` of a reason if it cannot.
+    pub fn can_add_piece(&self, position: Position,
+                         piece: &Piece,
+                         transformation: Transformation)
+                         -> Result<(), &'static str>
+    {
+        for p in piece.positions(transformation) {
+            let x = position.x + p.x;
+            let y = position.y + p.y;
+
+            if x >= self.dimension.width {
+                return Err("Piece hangs off right edge of board");
+            } else if y >= self.dimension.height {
+                return Err("Piece hangs off bottom edge of board");
+            } else if self.rows[y][x] {
+                return Err("Piece overlaps other piece");
+            }
+        }
+
+        Ok(())
     }
 
-    pub fn add_piece(&mut self, x: usize, y: usize, piece: Piece) -> Result<(), Piece> {
-        Err(piece)
+    /// Adds the given piece at the specified position under the given transformation.
+    ///
+    /// # Errors
+    ///
+    /// Panics if the piece cannot be added because it doesn't fit.
+    pub fn add_piece(&mut self, position: Position, piece: &Piece, transformation: Transformation) {
+        self.can_add_piece(position, piece, transformation).unwrap();
+
+        for p in piece.positions(transformation) {
+            self.rows[position.y + p.y][position.x + p.x] = true;
+        }
     }
 }
