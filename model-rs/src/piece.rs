ac1143cf1efb4b8d91e7dc6c6213be469d7192d8
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-29 23:37:15 -0500

    Transformations more correct now?

--- model-rs/src/piece.rs
+++ model-rs/src/piece.rs
@@ -12,47 +12,79 @@ pub struct Piece {
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum Transformation {
-    FlipHorizontal {
-        width: usize,
-    },
-    Rotation {
-        clockwise90s: usize,
-        width: usize,
-        height: usize,
+pub enum Rotation {
+    NoRotation,
+    Clockwise90,
+    Clockwise180,
+    Clockwise270,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Flip {
+    Identity,
+    Horizontal,
+}
+
+/// Ways that a game piece can be positioned.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct Transformation {
+    rotation: Rotation,
+    flip:     Flip
+}
+
+impl Rotation {
+    fn apply(self, width: usize, height: usize, x: usize, y: usize) -> (usize, usize) {
+        use self::Rotation::*;
+
+        match self {
+            NoRotation      => (x, y),
+            Clockwise90     => (height - y, x),
+            Clockwise180    => (width - x, height - y),
+            Clockwise270    => (y, width - x),
+        }
+    }
+}
+
+impl Flip {
+    pub fn apply(self, width: usize, height: usize, x: usize, y: usize) -> (usize, usize) {
+        use self::Flip::*;
+
+        match self {
+            Identity => (x, y),
+            Horizontal  => (width - x, y),
+        }
     }
 }
 
 impl Transformation {
-    pub fn apply(self, x: usize, y: usize) -> (usize, usize) {
-        match self {
-            Transformation::FlipHorizontal { width } => (width - x, y),
-            Transformation::Rotation { clockwise90s, width, height } => {
-                match clockwise90s % 4 {
-                    0 => (x, y),
-                    1 => (height - y, x),
-                    2 => (width - x, height - y),
-                    3 => (y, width - x),
-                    _ => unreachable!(),
-                }
-            }
-        }
-
+    pub fn apply(self, width: usize, height: usize, x: usize, y: usize) -> (usize, usize) {
+        let Transformation { rotation, flip } = self;
+        let rotated = rotation.apply(width, height, x, y);
+        flip.apply(width, height, rotated.0, rotated.1)
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct Positions<'a> {
     raw_positions: slice::Iter<'a, (usize, usize)>,
     transformation: Transformation,
+    width: usize,
+    height: usize,
 }
 
 impl Piece {
     pub fn width(&self, transformation: Transformation) -> usize {
-        transformation.apply(self.width, self.height).0
+        match transformation.rotation {
+            Rotation::NoRotation | Rotation::Clockwise180 => self.width,
+            Rotation::Clockwise90 | Rotation::Clockwise270 => self.height,
+        }
     }
 
     pub fn height(&self, transformation: Transformation) -> usize {
-        transformation.apply(self.width, self.height).1
+        match transformation.rotation {
+            Rotation::NoRotation | Rotation::Clockwise180 => self.height,
+            Rotation::Clockwise90 | Rotation::Clockwise270 => self.width,
+        }
     }
 
     pub fn cost(&self) -> usize {
@@ -67,6 +99,8 @@ impl Piece {
         Positions {
             raw_positions: (&*self.positions).into_iter(),
             transformation: transformation,
+            width: self.width,
+            height: self.height,
         }
     }
 
@@ -91,7 +125,8 @@ impl<'a> Iterator for Positions<'a> {
     type Item = (usize, usize);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.raw_positions.next().map(|&(x, y)| self.transformation.apply(x, y))
+        self.raw_positions.next().map(|&(x, y)|
+            self.transformation.apply(self.width, self.height, x, y))
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
